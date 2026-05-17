662d2ed6b73e8007cbda10254daf4b974b7af195
Sagar Saha <Sagarsaha2579@gmail.com>
2026-05-17 18:30:46 -0400

    Delete Product

--- Testing/Controllers/ProductController.cs
+++ Testing/Controllers/ProductController.cs
@@ -56,8 +56,11 @@ namespace Testing.Controllers
             return RedirectToAction("Index");
         }
 
-
-
+        public IActionResult DeleteProduct(Product product)
+        {
+            repo.DeleteProduct(product);
+            return RedirectToAction("Index");
+        }
 
     }
 }
